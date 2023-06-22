6b1edf891cd151e656abd054ce1d73e775205628
mikelangelo29 <michelangelozanelli@gmail.com>
2023-06-22 10:42:39 +0200

    change allowed

--- social_site/social_site/settings.py
+++ social_site/social_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k07hzrk@4bl#0bh!2se(tgu4o%hy*xj7*di(ufu4syo3^zjixh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
